e9f9ce66be37a6cbc2aa1a1e481ddb5b8d010439
Michael Merz <dev@telekobold.de>
2022-06-06 14:14:28 +0200

    Implement determining and printing window height and width and temporarily exiting and reentering ncurses mode

--- falling_chars.c
+++ falling_chars.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <ncurses.h>
 
+int width;
+int height;
+
 void get_chars();
 
 // Reads in the content of the file with the passed filename and prints it 
@@ -29,11 +32,18 @@ int main(int argc, char *argv[])
     getch(); // Wait for user input
     endwin(); // End ncurses mode
     */
-    
-    const char *filename = "test_file.txt";
-    
     initscr(); // Start ncurses mode
     
+    const char *filename = "test_file.txt";
+    width = getmaxx(stdscr);
+    height = getmaxy(stdscr);
+    def_prog_mode(); // Save the current terminal content
+    endwin();
+    printf("width = %d\n", width);
+    printf("height = %d\n", height);
+    getchar();
+    reset_prog_mode(); // Restore the saved terminal content
+    refresh();
     print_file_to_screen(filename);
     
     getch(); // Wait for user input so that the input is shown on the screen
