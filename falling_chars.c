a4bd6661c6e3221b6b1bf585735d268d0a253b32
Michael Merz <dev@telekobold.de>
2022-06-06 13:45:21 +0200

    Move printing file functionality to function

--- falling_chars.c
+++ falling_chars.c
@@ -3,6 +3,24 @@
 
 void get_chars();
 
+// Reads in the content of the file with the passed filename and prints it 
+// to the screen.
+int print_file_to_screen(const char *filename)
+{
+    FILE *textfile;
+    textfile = fopen(filename, "r");
+    if (textfile != NULL){
+        char *line = NULL;
+        size_t len = 0;
+        ssize_t read;
+        while((read = getline(&line, &len, textfile)) != -1)
+        {
+            //printf("%s", line);
+            printw("%s", line);
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     /*
@@ -13,19 +31,11 @@ int main(int argc, char *argv[])
     */
     
     const char *filename = "test_file.txt";
-    FILE *textfile;
-    textfile = fopen(filename, "r");
-    if (textfile != NULL){
-        initscr(); // Start ncurses mode
-        char *line = NULL;
-        size_t len = 0;
-        ssize_t read;
-        while((read = getline(&line, &len, textfile)) != -1)
-        {
-            //printf("%s", line);
-            printw("%s", line);
-        }
-        getch(); // Wait for user input so that the input is shown on the screen
-        endwin(); // End ncurses mode
-    }
+    
+    initscr(); // Start ncurses mode
+    
+    print_file_to_screen(filename);
+    
+    getch(); // Wait for user input so that the input is shown on the screen
+    endwin(); // End ncurses mode
 }
