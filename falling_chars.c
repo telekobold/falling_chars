6396af1ef8cd5bf898dfe5f66f31d897d8491877
Michael Merz <dev@telekobold.de>
2022-06-09 00:38:55 +0200

    Add randomness to selection of chars to fall down

--- falling_chars.c
+++ falling_chars.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <ncurses.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define SLEEP usleep(9000)
 
-typedef struct {
+typedef struct
+{
     int x; // The char's x position on the ncurses window
     int y; // The char's y position on the ncurses window
     char c; // The char's value
@@ -36,11 +39,41 @@ void print_file_to_screen(const char *filename)
     }
 }
 
+// Returns 1 if numbers contains number, 0 otherwise.
+unsigned contains(const unsigned *numbers, const unsigned numbers_size, const unsigned number)
+{
+    for(unsigned i = 0; i < numbers_size; i++)
+        if(numbers[i] == number)
+            return 1;
+    return 0;
+}
+
+// Returns a list of n numbers between 0 and n, randomly shuffled, but unique 
+// (meaning that each number appears only once in the list)
+// or `NULL` for n <= 0.
+void get_n_rand_numbers(unsigned n, unsigned *rand_numbers)
+{
+    if(n <= 0)
+        rand_numbers = NULL;
+    srand((unsigned) time(0)); // Seed rand()'s random number generator
+    unsigned lower = 0, upper = n;
+    for(unsigned i = 0; i < n; i++)
+    {
+        unsigned rand_num = (rand() % (upper - lower + 1)) + lower; // % upper + 1
+        // Avoid that the same number is added twice to the list:
+        while(contains(rand_numbers, n, rand_num))
+            rand_num = (rand() % (upper - lower + 1)) + lower; // % upper + 1
+        rand_numbers[i] = rand_num;
+    }
+}
+
 // Takes a char at an existing position on the standard window and lets it 
 // "fall down" until the char reached the bottom of the window or a stack of
 // one or more already fallen down characters at the bottom of the window.
-void let_char_fall_down(Pos_tuple char_pos){
-    for(int i = 0; char_pos.y+i < height-1; i++){
+void let_char_fall_down(Pos_tuple char_pos)
+{
+    for(unsigned i = 0; char_pos.y+i < height-1; i++)
+    {
         char next_char = mvinch(char_pos.y+i+1,char_pos.x) & A_CHARTEXT;
         if (next_char != ' ')
             break;
@@ -81,13 +114,16 @@ int main(int argc, char *argv[])
     //getch(); // TODO: To be removed in the production version
     Pos_tuple window_columns[width][height];
     
-    int non_space_char_count = 0;
-    int space_count = 0;
-    int i = 0, j = 0;
-    for(; i < width; i++){
-        for(j = 0; j < height; j++){
+    unsigned non_space_char_count = 0;
+    unsigned space_count = 0;
+    unsigned i = 0, j = 0;
+    for(; i < width; i++)
+    {
+        for(j = 0; j < height; j++)
+        {
             char c = mvinch(j,i) & A_CHARTEXT;
-            if(c != ' '){
+            if(c != ' ')
+            {
                 /*
                 def_prog_mode();
                 endwin();
@@ -108,7 +144,8 @@ int main(int argc, char *argv[])
             } else 
                 space_count++;
         }
-        if(j < height){
+        if(j < height)
+        {
             Pos_tuple tuple = {-1, -1, '\0'};
             window_columns[i][j] = tuple;
         }
@@ -124,10 +161,17 @@ int main(int argc, char *argv[])
     
     //if (test_output_file != NULL){
         //for(int i = 0; width; i++){
-        for(i = 0; i < 21; i++){
+        //for(i = 0; i < 21; i++){
+        for(i = 1; i >= 0; i--)
+        {
             //for(int j = 0; window_columns[i][j].x != -1 && window_columns[i][j].y != -1 && window_columns[i][j].c != '\0'; j++){
-            for(j = 1; j >=0; j--){
-                let_char_fall_down(window_columns[i][j]);
+            //for(j = 1; j >=0; j--){
+            unsigned n = 21;
+            unsigned n_rand_numbers[n];
+            get_n_rand_numbers(n, n_rand_numbers);
+            for(j = 0; j < n; j++)
+            {
+                let_char_fall_down(window_columns[n_rand_numbers[j]][i]);
                 //printf("Char at position (%d, %d) = %c\n", window_columns[i][j].x, window_columns[i][j].y, window_columns[i][j].c);
                 //fprintf(test_output_file, "Char at position (%d, %d): %c\n", window_columns[i][j].x, window_columns[i][j].y, window_columns[i][j].c);
                 //linecount++;
