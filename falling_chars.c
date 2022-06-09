a79494187e0685b39943d0a25aade39e06320465
Michael Merz <dev@telekobold.de>
2022-06-09 23:32:42 +0200

    Delete commented out code; remove unneccesary parentheses; small change in last loop

--- falling_chars.c
+++ falling_chars.c
@@ -27,15 +27,7 @@ void print_file_to_screen(const char *filename)
         size_t len = 0;
         ssize_t read;
         while((read = getline(&line, &len, textfile)) != -1)
-        {
             printw("%s", line);
-            // Control output:
-            def_prog_mode();
-            endwin();
-            printf("%s", line);
-            reset_prog_mode();
-            refresh();
-        }
     }
 }
 
@@ -77,7 +69,9 @@ void let_char_fall_down(Pos_tuple char_pos)
         char next_char = mvinch(char_pos.y+i+1,char_pos.x) & A_CHARTEXT;
         if (next_char != ' ')
             break;
+        // Overwrite the previous position of the char:
         mvaddch(char_pos.y+i, char_pos.x, ' ');
+        // ...and write it one y-position ahead:
         mvaddch(char_pos.y+i+1, char_pos.x, char_pos.c);
         refresh();
         SLEEP;
@@ -88,34 +82,13 @@ int main(int argc, char *argv[])
 {
     initscr(); // Start ncurses mode
     
-    // Control output:
     const char *filename = "test_file_2.txt";
-    const char *test_output_filename = "test_output.txt";
-    //FILE *test_output_file;
-    //test_output_file = fopen(test_output_filename, "w");
-    
     width = getmaxx(stdscr); // = number of columns
     height = getmaxy(stdscr);
-    /*
-    def_prog_mode(); // Save the current terminal content
-    endwin();
-    printf("width = %d\n", width);
-    printf("height = %d\n", height);
-    if (test_output_file != NULL){
-        fprintf(test_output_file, "width = %d\n", width);
-        fprintf(test_output_file, "height = %d\n", height);
-    }
-    getchar();
-    reset_prog_mode(); // Restore the saved terminal content
-    refresh();
-    */
-    
-    print_file_to_screen(filename);
-    //getch(); // TODO: To be removed in the production version
     Pos_tuple window_columns[width][height];
     
-    unsigned non_space_char_count = 0;
-    unsigned space_count = 0;
+    print_file_to_screen(filename);
+    
     unsigned i = 0, j = 0;
     for(; i < width; i++)
     {
@@ -124,25 +97,9 @@ int main(int argc, char *argv[])
             char c = mvinch(j,i) & A_CHARTEXT;
             if(c != ' ')
             {
-                /*
-                def_prog_mode();
-                endwin();
-                printf("Detected non-space char %c\n", c);
-                reset_prog_mode();
-                refresh();
-                */
-                non_space_char_count++;
                 Pos_tuple tuple = {i, j, c};
-                /*
-                def_prog_mode();
-                endwin();
-                printf("Adding new Pos_tuple {%d, %d, %c}\n", i, j, c);
-                reset_prog_mode();
-                refresh();
-                */
                 window_columns[i][j] = tuple;
-            } else 
-                space_count++;
+            }
         }
         if(j < height)
         {
@@ -150,42 +107,16 @@ int main(int argc, char *argv[])
             window_columns[i][j] = tuple;
         }
     }
-    //endwin();
-    /*
-    // Control output:
-    printf("i = %d\n", i);
-    printf("j = %d\n", j);
-    printf("The window contains %d non-space chars.\n", non_space_char_count);
-    printf("The window contains %d spaces.\n", space_count);
-    */
     
-    //if (test_output_file != NULL){
-        //for(int i = 0; width; i++){
-        //for(i = 0; i < 21; i++){
-        for(i = 1; i >= 0; i--)
-        {
-            //for(int j = 0; window_columns[i][j].x != -1 && window_columns[i][j].y != -1 && window_columns[i][j].c != '\0'; j++){
-            //for(j = 1; j >=0; j--){
-            unsigned n = 21;
-            unsigned n_rand_numbers[n];
-            get_n_rand_numbers(n, n_rand_numbers);
-            for(j = 0; j < n; j++)
-            {
-                let_char_fall_down(window_columns[n_rand_numbers[j]][i]);
-                //printf("Char at position (%d, %d) = %c\n", window_columns[i][j].x, window_columns[i][j].y, window_columns[i][j].c);
-                //fprintf(test_output_file, "Char at position (%d, %d): %c\n", window_columns[i][j].x, window_columns[i][j].y, window_columns[i][j].c);
-                //linecount++;
-            }
-        }
-    //}
-    /*
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 22; j++){
-            char c = mvinch(i,j) & A_CHARTEXT;
-            printf("mvinch(%d, %d) = %c\n", i ,j, c);
-        }
+    
+    for(i = 1; i >= 0; i--)
+    {
+        unsigned n = 21;
+        unsigned n_rand_numbers[n];
+        get_n_rand_numbers(n, n_rand_numbers);
+        for(j = 0; j < n; j++)
+            let_char_fall_down(window_columns[n_rand_numbers[j]][i]);
     }
-    */
     pause(); // Sleep forever, e.g. until Ctrl. + C is activated.
     //endwin();
 }
